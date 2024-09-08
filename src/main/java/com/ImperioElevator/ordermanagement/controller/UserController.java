48147a6e954b0e35d996995608c6859739183b14
mcardauc <cardaucmihai@gmail.com>
2024-09-09 00:16:26 +0300

    Added some changes for the User

--- src/main/java/com/ImperioElevator/ordermanagement/controller/UserController.java
+++ src/main/java/com/ImperioElevator/ordermanagement/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.ImperioElevator.ordermanagement.controller;
 
 import com.ImperioElevator.ordermanagement.dao.daoimpl.ProductDaoImpl;
+import com.ImperioElevator.ordermanagement.entity.EntityCreationResponse;
 import com.ImperioElevator.ordermanagement.entity.User;
 import com.ImperioElevator.ordermanagement.enumobects.Role;
 import com.ImperioElevator.ordermanagement.service.EmailService;
@@ -10,6 +11,8 @@ import com.ImperioElevator.ordermanagement.valueobjects.Id;
 import liquibase.sql.Sql;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,8 +33,14 @@ public class UserController {
     }
     //ToDo un response normal
     @PostMapping("createUser/Superior")
-    public Long addNewUserSuperior(@RequestBody User user)throws SQLException{
-        return userSevice.addNewUser(user);
+    public ResponseEntity<EntityCreationResponse> addNewUserSuperior(@RequestBody User user)throws SQLException{
+          Long userId = userSevice.addNewUser(user);
+          EntityCreationResponse response = new EntityCreationResponse(
+                  userId,
+                  "User " + userId + " was added successfully"
+          );
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+
     }
 
     @PostMapping("/sendMail/confirm/user/{token}")
