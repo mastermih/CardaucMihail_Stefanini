3f17e0e53684b620dfe22476eb7d52dff46496f8
mcardauc <cardaucmihai@gmail.com>
2024-10-17 14:41:20 +0300

    Added the application.docker.prop for Docker so i do not have to comant th app prop also made some changs in the yml for docker to ix the problem with the front from the docker container

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,8 @@ WORKDIR /app
 
 COPY target/order_management-1.0-SNAPSHOT.jar /app/order_management-1.0-SNAPSHOT.jar
 
+ENV SPRING_PROFILES_ACTIVE=docker
+
 EXPOSE 8080
 
 ENTRYPOINT ["java", "-jar", "/app/order_management-1.0-SNAPSHOT.jar"]
--- src/main/java/com/ImperioElevator/ordermanagement/config/WebConfig.java
+++ src/main/java/com/ImperioElevator/ordermanagement/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001")  // Allow both ports
+                .allowedOrigins("http://localhost:3000", "http://localhost:57232")  // Allow both ports
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
